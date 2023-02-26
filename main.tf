9671cf344b0b841211f729480cb6321e0801f3ae
jimmyscode <89364456+jimmy-GHub@users.noreply.github.com>
2023-02-27 10:48:51 +1300

    Updater

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
-resource "azurerm_resource_group" "rg" {
-  location = var.location
-  name = "rg_original"
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "1-07292168-playground-sandbox"
+}
+
+resource "random_id" "rand" {
+  byte_length = 10
 }
 
 module "storage_accounts" {
   source = "./modules/storage"
-  for_each = locals.storage
-  st_name = each.key
+  #for_each = local.storage
+  st_name = "storeme${lower(random_id.rand)}"
   account_replication_type = "GRS"
   st_tier = "Standard"
   location = var.location
-  rg_name = azurerm_resource_group.rg.name
-}
+  rg_name = data.azurerm_resource_group.rg.name
+  }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "rando_string" {
+  value = module.storage_accounts.st_name
+}
